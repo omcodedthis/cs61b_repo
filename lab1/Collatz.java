2a4a3b35d577f4698b9504ec5a139f04a71b38d1
oM <omtirodkar03@gmail.com>
2023-04-06 16:11:34 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,16 +1,18 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author om
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** nextNumber() first determines whether n is positive or negative. If n is even, n/2 is returned. If n is
+     odd, 3n + 1 is returned. */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        int remainder = n % 2;
+        if (remainder == 0) {
+            return (n / 2);
+        }
+
+        else {
+            return (3 * n) + 1;
         }
     }
 
