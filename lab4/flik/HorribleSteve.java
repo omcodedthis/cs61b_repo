4e20bc27205389f2ed8113b4b040fae8a4c715ef
oM <omtirodkar03@gmail.com>
2023-05-01 20:00:31 +0800

    done with lab4

--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -1,9 +1,10 @@
 package flik;
 
 /** Why does 128 == 128 return false?:
- *  This is due to memory optimisations in Java, one of which is autoboxing. Values from -128 to 127 are cached, values
- *  outside these range aren't cached. This leads to values outside this range from not being cached, leading to
- *  comparison errors. The best way is to always compare values using .equals, which what I have implemented in Flik. */
+ *  This is due to memory optimisations in Java, one of which is autoboxing. Integer (reference type of int) Values
+ *  from -128 to 127 are cached, values outside these range aren't cached. This leads to values outside this range from
+ *  not being cached, leading to comparison errors. The best way is to always compare values using .equals, which what
+ *  I have implemented in Flik or declare the parameters as the primitive int type. */
 
 public class HorribleSteve {
     public static void main(String [] args) throws Exception {
