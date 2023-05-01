fc8b251aa447da4b5b91a9ed0cbd7936c3237bed
oM <119602009+omcodedthis@users.noreply.github.com>
2023-05-01 19:22:38 +0800

    buggy implementation for 4A

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -18,12 +18,7 @@ public class Collatz {
 
     public static void main(String[] args) {
         int n = 5;
-        System.out.print(n + " ");
-        while (n != 1) {
-            n = nextNumber(n);
-            System.out.print(n + " ");
-        }
-        System.out.println();
+        System.out.print("5 16 32 64 128 1");
     }
 }
 
