4471a3fb174332b0dbcec7a113ca4cf6445222ac
oM <119602009+omcodedthis@users.noreply.github.com>
2023-05-01 19:29:56 +0800

    checked out Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -18,7 +18,7 @@ public class Collatz {
 
     public static void main(String[] args) {
         int n = 5;
-        System.out.print("5 16 32 64 128 1");
+        System.out.print(n + " ");
         while (n != 1) {
             n = nextNumber(n);
             System.out.print(n + " ");
