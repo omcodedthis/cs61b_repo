56c62fec9a5a2ab4dd48d64a1b268e37bbe0cef3
oM <omtirodkar03@gmail.com>
2023-05-02 13:30:38 +0800

    done with EC

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -11,67 +11,66 @@ public class TestArrayDequeEC {
             ArrayDequeSolution<Integer> solArray = new ArrayDequeSolution<>();
             StudentArrayDeque<Integer> testArray = new StudentArrayDeque<>();
 
-            for (int i = 0; i < 100; i++) {
-                solArray.addLast(i);
-                testArray.addLast(i);
-                System.out.println("addLast(" + i +")");
+        for (int i = 0; i < 100; i++) {
+            solArray.addLast(i);
+            testArray.addLast(i);
+            System.out.println("addLast(" + i +")");
+        }
+
+        int test = StdRandom.uniform(4);
+        int cnt = 0;
+
+        while (true) {
+
+            if (test == 0) {
+                cnt++;
+
+                solArray.addFirst(44);
+                testArray.addFirst(44);
+
+                Integer expected = solArray.get(cnt);
+                Integer actual = testArray.get(cnt);
+
+                System.out.println("addFirst(44)");
+
+                assertEquals("addFirst(44)", expected, actual);
             }
 
-            int test = StdRandom.uniform(4);
-            int cnt = 0;
+            else if (test == 1) {
+                cnt++;
 
-            while (true) {
+                solArray.addLast(44);
+                testArray.addLast(44);
 
-                if (test == 0) {
-                    cnt++;
+                Integer expected = solArray.get(cnt);
+                Integer actual = testArray.get(cnt);
 
-                    solArray.addFirst(44);
-                    testArray.addFirst(44);
+                System.out.println("addLast(44)");
 
-                    Integer expected = solArray.get(cnt);
-                    Integer actual = testArray.get(cnt);
-
-                    System.out.println("addFirst(44)");
-
-                    assertEquals("addFirst(44)", expected, actual);
-                }
-
-                else if (test == 1) {
-                    cnt++;
-
-                    solArray.addLast(44);
-                    testArray.addLast(44);
-
-                    Integer expected = solArray.get(cnt);
-                    Integer actual = testArray.get(cnt);
-
-                    System.out.println("addLast(44)");
-
-                    assertEquals("addLast(44)", expected, actual);
-
-                }
-                else if (test == 2) {
-                    cnt++;
-
-                    Integer expected = solArray.removeFirst();
-                    Integer actual = testArray.removeFirst();
-
-                    System.out.println("removeFirst()");
-
-                    assertEquals("removeFirst()", expected, actual);
-                }
-
-                else if (test == 3) {
-                    cnt++;
-
-                    Integer expected = solArray.removeLast();
-                    Integer actual = testArray.removeLast();
-
-                    System.out.println("removeLast()");
-
-                    assertEquals("removeLast()", expected, actual);
-                }
+                assertEquals("addLast(44)", expected, actual);
 
             }
+            else if (test == 2) {
+                cnt++;
+
+                Integer expected = solArray.removeFirst();
+                Integer actual = testArray.removeFirst();
+
+                System.out.println("removeFirst()");
+
+                assertEquals("removeFirst()", expected, actual);
+            }
+
+            else if (test == 3) {
+                cnt++;
+
+                Integer expected = solArray.removeLast();
+                Integer actual = testArray.removeLast();
+
+                System.out.println("removeLast()");
+
+                assertEquals("removeLast()", expected, actual);
+            }
+        }
     }
 }
