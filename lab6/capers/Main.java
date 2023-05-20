6cefe3a2911ed500f3e58831f2c2bb2867527b97
oM <omtirodkar03@gmail.com>
2023-05-20 15:32:29 +0800

    working on lab 6

--- lab6/capers/Main.java
+++ lab6/capers/Main.java
@@ -1,7 +1,7 @@
 package capers;
 
 import java.io.File;
-
+import java.util.Arrays;
 import static capers.Utils.*;
 
 /** Canine Capers: A Gitlet Prelude.
@@ -40,6 +40,7 @@ public class Main {
         if (args.length == 0) {
             Utils.exitWithError("Must have at least one argument");
         }
+        System.out.println("args: " + Arrays.toString(args));
 
         CapersRepository.setupPersistence();
         String text;
