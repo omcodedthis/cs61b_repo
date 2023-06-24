1b0aa99fb555454ad27eeb8b616eb2764832024b
oM <omtirodkar03@gmail.com>
2023-06-24 17:17:41 +0800

    done with lab 12

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -12,13 +12,81 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
-    public void addHexagon(int s) {
-        int lengthOfHexBlock = s * 2;
+    /** nested class that keeps track of the position of the Hexagon. */
+    private static class Postion {
+        public int xPos;
 
-        for (int i = 0; i < lengthOfHexBlock; i++) {
-            for (int j = 0; i < lengthOfHexBlock; i++) {
+        public int yPos;
 
+        private Postion(int x, int y) {
+            xPos = x;
+            yPos = y;
+        }
+    }
+
+
+    private static final int WIDTH = 50;
+    private static final int HEIGHT = 50;
+
+    private static final long SEED = 2873123;
+    private static final Random RANDOM = new Random(SEED);
+
+    /**
+     * Fills the given 2D array of tiles with NOTHING (blank) tiles.
+     */
+    public static void fillWithNothingTiles(TETile[][] tiles) {
+        int height = tiles[0].length;
+        int width = tiles.length;
+        for (int x = 0; x < width; x += 1) {
+            for (int y = 0; y < height; y += 1) {
+                tiles[x][y] = Tileset.NOTHING;
             }
         }
+    }
+
+
+    /** Starts the recursive call to drawHexActual(). */
+    public static void drawHex(int s, TETile[][] tiles, Postion hexPos) {
+        drawHexActual(s - 1, s, tiles, hexPos);
+    }
+
+
+    /** Recursively draws the hexagon. */
+    public static void drawHexActual(int blanks, int notBlanks, TETile[][] tiles, Postion hexPos) {
+        drawRow(blanks, notBlanks, tiles, hexPos);
+
+        if (blanks > 0) {
+            drawHexActual(blanks - 1, notBlanks + 2, tiles, hexPos);
+        }
+
+        drawRow(blanks, notBlanks, tiles, hexPos);
+    }
+
+
+    /** Draws a single row, used in the recursive calls of drawHexActual(). */
+    public static void drawRow(int blanks, int notBlanks, TETile[][] tiles, Postion hexPos) {
+        int initalX = hexPos.xPos;
+        hexPos.xPos += blanks;
+
+        for (int dx = 0; dx < notBlanks; dx++) {
+            tiles[hexPos.xPos + dx][hexPos.yPos] = Tileset.FLOWER;
+        }
+
+        hexPos.xPos = initalX;
+        hexPos.yPos--;
+    }
+
+
+    public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        TETile[][] randomTiles = new TETile[WIDTH][HEIGHT];
+        fillWithNothingTiles(randomTiles);
+
+        Postion hexPos = new Postion(15, 25);
+        drawHex(8, randomTiles, hexPos);
+
+        ter.renderFrame(randomTiles);
     }
 }
--- proj3/byow/lab12/project3prep.md
+++ proj3/byow/lab12/project3prep.md
@@ -5,6 +5,9 @@ After looking at your own implementation, consider the implementation provided n
 How did your implementation differ from the given one? What lessons can be learned from it?**
 
 Answer:
+My initial implementation was similar to tactical programming, just creating that something that had worked. However,
+by adopting the steps of strategic programming, I took into consideration the long-term structure of the system. I did 
+this by creating a nested class that accounted for the position & using a recursive function to draw the hexagon.
 
 -----
 
@@ -12,14 +15,17 @@ Answer:
 What is the hexagon and what is the tesselation on the Project 3 side?**
 
 Answer:
+The hexagon is the rooms & hallways and the tesselation is creating a world filled with these rooms & hallways.
 
 -----
 **If you were to start working on world generation, what kind of method would you think of writing first? 
 Think back to the lab and the process used to eventually get to tessellating hexagons.**
 
 Answer:
-
+I think I would focus on writing a method that generates a room & a hallway itself respectively.
 -----
 **What distinguishes a hallway from a room? How are they similar?**
 
 Answer:
+A hallway is a straight line, whereas a room is more of a rectangle in terms of general shape. Both are similar in the 
+way that both have an opening where the player can enter through.
