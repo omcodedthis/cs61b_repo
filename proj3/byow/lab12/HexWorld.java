d089ad3ff3a9dac98c751f36e7235cf3f668ccf5
oM <omtirodkar03@gmail.com>
2023-06-24 13:02:43 +0800

    proj3 getting started

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -12,4 +12,13 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    public void addHexagon(int s) {
+        int lengthOfHexBlock = s * 2;
+
+        for (int i = 0; i < lengthOfHexBlock; i++) {
+            for (int j = 0; i < lengthOfHexBlock; i++) {
+
+            }
+        }
+    }
 }
